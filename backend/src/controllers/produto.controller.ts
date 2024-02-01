b408815cfcff5ca7f4580046ebfd169ff2d91cfc
joao <joao.menna@innovo.com.br>
2024-02-01 07:29:38 -0300

    (backend) feito scaffolding dos controllers e renomeado routes para convencao

---
+++ backend/src/controllers/produto.controller.ts
@@ -0,0 +1,15 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export class ProdutoController {
+  async getAllByRestauranteId (req: FastifyRequest, rep: FastifyReply): Promise<void> {
+  }
+
+  async createOne (req: FastifyRequest, rep: FastifyReply): Promise<void> {
+  }
+
+  async updateOne (req: FastifyRequest, rep: FastifyReply): Promise<void> {
+  }
+
+  async deleteOne (req: FastifyRequest, rep: FastifyReply): Promise<void> {
+  }
+}
