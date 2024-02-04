18ca6d2ba64e6bf6f780cd8d007b238cecbf2294
NomikuH8 <sofaco.checkin@gmail.com>
2024-02-04 16:48:49 -0300

    (backend) feito model de horarios de promocao, nao testado

--- backend/src/models/produto.model.ts
+++ backend/src/models/produto.model.ts
@@ -1,10 +1,12 @@
 import { sql } from 'drizzle-orm'
-import { produtoTable } from '../database/schemas/produto'
+import { produtoPromocaoHorarioTable, produtoTable } from '../database/schemas/produto'
 import { RowList } from 'postgres'
 import { db } from '../database/connection'
 
 type ProdutoType = typeof produtoTable.$inferSelect
 type ProdutoInsertType = typeof produtoTable.$inferInsert
+type HorarioType = typeof produtoPromocaoHorarioTable.$inferSelect
+type HorarioInsertType = typeof produtoPromocaoHorarioTable.$inferInsert
 
 export class ProdutoModel {
   async getAllFromRestaurante (restauranteId: number): Promise<RowList<ProdutoType[]>> {
@@ -29,6 +31,31 @@ export class ProdutoModel {
     return await db.execute(insert)
   }
 
+  async insertMultiplePromocionalHorarios (restauranteId: number, horarios: HorarioInsertType[]): Promise<RowList<HorarioType[]>> {
+    const insert = sql`
+      INSERT INTO ${produtoPromocaoHorarioTable} (
+        ${produtoPromocaoHorarioTable.produtoId},
+        ${produtoPromocaoHorarioTable.diaSemana},
+        ${produtoPromocaoHorarioTable.de},
+        ${produtoPromocaoHorarioTable.ate}
+      ) VALUES
+    `
+
+    for (const horario of horarios) {
+      insert.append(sql` (${restauranteId}, ${horario.diaSemana}, ${horario.de}, ${horario.ate})`)
+
+      const index = horarios.findIndex((val) => val.diaSemana === horario.diaSemana)
+      if (index < horarios.length - 1) {
+        insert.append(sql`,`)
+      }
+    }
+
+    insert.append(sql` RETURNING *`)
+
+    const newHorarios = await db.execute<HorarioType>(insert)
+    return newHorarios
+  }
+
   async updateProduto (produtoId: number, produto: ProdutoInsertType): Promise<RowList<ProdutoType[]>> {
     const update = sql`
       UPDATE ${produtoTable}
@@ -43,10 +70,34 @@ export class ProdutoModel {
     return await db.execute(update)
   }
 
+  async updateProdutoPromocaoHorario (produtoId: number, horarioId: number, horario: HorarioInsertType): Promise<RowList<HorarioType[]>> {
+    const update = sql`
+      UPDATE ${produtoPromocaoHorarioTable}
+      SET
+        ${produtoPromocaoHorarioTable.de} = ${horario.de},
+        ${produtoPromocaoHorarioTable.ate} = ${horario.ate}
+      WHERE ${produtoPromocaoHorarioTable.produtoId} = ${produtoId}
+            AND ${produtoPromocaoHorarioTable.id} = ${horarioId}
+      RETURNING *
+    `
+
+    const updatedHorario = await db.execute<HorarioType>(update)
+    return updatedHorario
+  }
+
   async deleteProduto (produtoId: number): Promise<void> {
     const deleteQuery = sql`
       DELETE FROM ${produtoTable}
       WHERE ${produtoTable.id} = ${produtoId}
+    `
+
+    await db.execute(deleteQuery)
+  }
+
+  async deleteMultiplePromocaoHorarios (produtoId: number): Promise<void> {
+    const deleteQuery = sql`
+      DELETE FROM ${produtoPromocaoHorarioTable}
+      WHERE ${produtoPromocaoHorarioTable.produtoId} = ${produtoId}
     `
 
     await db.execute(deleteQuery)
